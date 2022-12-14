d187c5401eb20d08fb38beef4faceb4334fd3d06
iorin006 <108205818+iorin006@users.noreply.github.com>
2022-12-14 15:48:18 +0900

    Add files via upload

--- time.js
+++ time.js
@@ -1,5 +1,7 @@
 var id = document.getElementById('datenow')
 var id2 = document.getElementById('datenow2')
+var id3 = document.getElementById('cm')
+var aa=45
 function nowdate() {
     var now = new Date()
     var year = now.getFullYear()
@@ -11,23 +13,23 @@ function nowdate() {
     let day_arr = ['(日)', '(月)', '(火)', '(水)', '(木)', '(金)', '(土)']
     let day = day_arr[now.getDay()];
 
-    if (minutes < 9) {
+    if (minutes <= 9) {
         minutes = '0' + minutes
     }
-    if (seconds < 9) {
+    if (seconds <= 9) {
         seconds = '0' + seconds
     }
-    if (hour < 9) {
+    if (hour <= 9) {
         hour = '0' + hour
     }
-    id.innerHTML = year + '年' + month + '月' + date1 + '日' + day
-    id2.innerHTML = hour + ':' + minutes + ':' + seconds
+    id.innerText = year + '年' + month + '月' + date1 + '日' + day
+    id2.innerText = hour + ':' + minutes + ':' + seconds
 
     var obj = document.getElementById("datenow2");
-    obj.style.color = '#c71585';
+
     obj.style.color = '#191970';
+
 }
 
 
-
-setInterval(nowdate, 0)
+ setInterval(nowdate, 500)
