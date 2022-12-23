e71a9725ab2c7017ba8615cc03eefb037d2bdafd
iorin006 <108205818+iorin006@users.noreply.github.com>
2022-12-23 13:05:55 +0900

    Add files via upload

--- time.js
+++ time.js
@@ -1,7 +1,7 @@
 var id = document.getElementById('datenow')
 var id2 = document.getElementById('datenow2')
 var id3 = document.getElementById('cm')
-var aa=45
+var down = 0
 function nowdate() {
     var now = new Date()
     var year = now.getFullYear()
@@ -24,12 +24,31 @@ function nowdate() {
     }
     id.innerText = year + '年' + month + '月' + date1 + '日' + day
     id2.innerText = hour + ':' + minutes + ':' + seconds
-
-    var obj = document.getElementById("datenow2");
-
-    obj.style.color = '#191970';
-
+    id2.style.color = '#191970';
+    if (id2.innerText >= '09:00:00') {
+        cm.innerText = '朝礼まで'
+    }
+    if (id2.innerText >= '09:45:00') {
+        cm.innerText = '1限目'
+        //cm.innerText='授業終了まで'
+    }
+    if (id2.innerText >= '10:45') {
+        cm.innerText = '2限目'
+    }
+    if (id2.innerText >= '11:45:00') {
+        cm.innerText = '3限目'
+    }
+    if (id2.innerText >= '12:35:00') {
+        cm.innerText = '昼休み'
+    }
+    if (id2.innerText >= '13:15:00') {
+        cm.innerText = '4限目'
+    }
+    if (id2.innerText >= '14:15:00') {
+        cm.innerText = '5限目'
+    }
+    if (id2.innerText >= '15:15:00') {
+        cm.innerText = '6限目'
+    }
 }
-
-
- setInterval(nowdate, 0)
+setInterval(nowdate, 0)
