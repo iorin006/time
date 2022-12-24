93dc20a7ebf80cf5b2fc757f7f96ade2c2a8f833
iorin006 <108205818+iorin006@users.noreply.github.com>
2022-12-24 15:30:05 +0900

    Add files via upload

--- time.js
+++ time.js
@@ -2,6 +2,10 @@ var id = document.getElementById('datenow')
 var id2 = document.getElementById('datenow2')
 var id3 = document.getElementById('cm')
 var down = 0
+
+console.log('使ってくれてありがとうございます')
+console.log('なんか良いアイディアあれば教えてください')
+console.log('よろしくお願いします')
 function nowdate() {
     var now = new Date()
     var year = now.getFullYear()
@@ -25,30 +29,58 @@ function nowdate() {
     id.innerText = year + '年' + month + '月' + date1 + '日' + day
     id2.innerText = hour + ':' + minutes + ':' + seconds
     id2.style.color = '#191970';
+
+
     if (id2.innerText >= '09:00:00') {
         cm.innerText = '朝礼まで'
     }
+    //１
     if (id2.innerText >= '09:45:00') {
         cm.innerText = '1限目'
-        //cm.innerText='授業終了まで'
     }
-    if (id2.innerText >= '10:45') {
+    if (id2.innerText >= '10:35:00') {
+        cm.innerText = "休み時間"
+    }
+    //2
+    if (id2.innerText >= '10:45:00') {
         cm.innerText = '2限目'
     }
+    if (id2.innerText >= '11:35:00') {
+        cm.innerText = "休み時間"
+    }
+    //3
     if (id2.innerText >= '11:45:00') {
         cm.innerText = '3限目'
     }
     if (id2.innerText >= '12:35:00') {
         cm.innerText = '昼休み'
     }
+    //4
     if (id2.innerText >= '13:15:00') {
         cm.innerText = '4限目'
     }
+    if (id2.innerText >= '14:05:00') {
+        cm.innerText = "休み時間"
+    }
+    //5
     if (id2.innerText >= '14:15:00') {
         cm.innerText = '5限目'
     }
+    if (id2.innerText >= '15:05') {
+        cm.innerText = "休み時間"
+    }
+    //6
     if (id2.innerText >= '15:15:00') {
         cm.innerText = '6限目'
+    }
+    if (id2.innerText >= '16:05:00') {
+        cm.innerText = "終礼"
+    }
+    if (id2.innerText >= '16:15:00') {
+        cm.innerText = "放課後"
+    }
+    if (id2.innerText >= '17:30:00') {
+        cm.innerText = "解散"
     }
 }
 setInterval(nowdate, 0)
